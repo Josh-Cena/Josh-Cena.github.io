34873194921a7d109477ba9b4f5c7b7efe1fb95b
Joshua Chen <sidachen2003@gmail.com>
2023-10-30 10:08:22 -0400

    Refactor scrolly with intersection observer

--- src/components/Scrolly/Elements.tsx
+++ src/components/Scrolly/Elements.tsx
@@ -7,46 +7,55 @@ const cCenterOrig = { x: 137.853, y: 128.715 };
 const jSizeOrig = { w: 158.932, h: 143.49 };
 const cSizeOrig = { w: 75.67, h: 80.026 };
 
-function ScrollyElements({
-  superTall,
-  gScale,
-  gShift,
-  jCenter,
-  jSize,
-  jColor,
-  cCenter,
-  cSize,
-  text1,
-  text2,
-  text1Pos,
-  text2Pos,
-  textSize,
-  text1Opacity,
-  text2Opacity,
-  borderOpacity,
-  borderShiftY,
-}: {
-  superTall: boolean;
-  gScale: number;
-  gShift: { x: number; y: number};
-  jCenter: { x: number; y: number };
-  jSize: { w: number; h: number };
-  jColor: number;
-  cCenter: { x: number; y: number };
-  cSize: { w: number; h: number };
-  text1: string;
-  text2: string;
-  text1Pos: { x: number; y: number };
-  text2Pos: { x: number; y: number };
-  textSize: number;
-  text1Opacity: number;
-  text2Opacity: number;
-  borderOpacity: number;
-  borderShiftY: number;
-}, ref: React.Ref<SVGSVGElement>) {
+function ScrollyElements(
+  {
+    superTall,
+    gScale,
+    gShift,
+    jCenter,
+    jSize,
+    jColor,
+    cCenter,
+    cSize,
+    text1,
+    text2,
+    text1Pos,
+    text2Pos,
+    textSize,
+    text1Opacity,
+    text2Opacity,
+    borderProgress,
+    borderShiftY,
+  }: {
+    superTall: boolean;
+    gScale: number;
+    gShift: { x: number; y: number };
+    jCenter: { x: number; y: number };
+    jSize: { w: number; h: number };
+    jColor: number;
+    cCenter: { x: number; y: number };
+    cSize: { w: number; h: number };
+    text1: string;
+    text2: string;
+    text1Pos: { x: number; y: number };
+    text2Pos: { x: number; y: number };
+    textSize: number;
+    text1Opacity: number;
+    text2Opacity: number;
+    borderProgress: number;
+    borderShiftY: number;
+  },
+  ref: React.Ref<SVGSVGElement>,
+) {
   return (
-    <svg ref={ref} xmlns="http://www.w3.org/2000/svg" viewBox={`-100 0 400 ${superTall ? 1000 : 200}`} className={clsx(styles.elements)} preserveAspectRatio="xMinYMin slice">
+    <svg
+      ref={ref}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`-100 0 400 ${superTall ? 1000 : 200}`}
+      className={clsx(styles.elements)}
+      preserveAspectRatio="xMinYMin slice">
       <g
+        data-color-mode="dark"
         transform={`matrix(${gScale}, 0, 0, ${gScale}, ${gShift.x}, ${gShift.y})`}>
         <path
           transform={`matrix(${jSize.w / jSizeOrig.w}, 0, 0, ${
@@ -54,7 +63,8 @@ function ScrollyElements({
           }, ${jCenter.x - (jCenterOrig.x * jSize.w) / jSizeOrig.w}, ${
             jCenter.y - (jCenterOrig.y * jSize.h) / jSizeOrig.h
           })`}
-          fill={`rgb(${jColor} ${jColor} ${jColor})`}
+          fill="var(--color-text)"
+          filter={`brightness(${jColor})`}
           d="m 25.000024,33.983972 c 0,6.666603 0,13.333209 0,19.999811 21.206457,0 42.412914,0 63.619371,0 C 76.268383,84.308203 63.917371,114.63262 51.56636,144.95704 43.949254,138.6582 36.332149,132.35936 28.715044,126.06052 c -4.215592,5.09771 -8.431183,10.19542 -12.646774,15.29312 14.560523,12.04027 29.121047,24.08053 43.68157,36.1208 16.765902,-41.16356 33.531802,-82.327105 50.2977,-123.490657 21.65088,0 43.30175,0 64.95263,0 0,-6.666602 0,-13.333208 0,-19.999811 -50.00005,0 -100.000099,0 -150.000146,0 z"
         />
         <path
@@ -83,7 +93,9 @@ function ScrollyElements({
           {text2}
         </text>
       </g>
-      <g opacity={borderOpacity} transform={`matrix(1, 0, 0, 1, 0, ${borderShiftY})`}>
+      <g
+        opacity={borderProgress}
+        transform={`matrix(1, 0, 0, 1, 0, ${borderShiftY})`}>
         <path
           stroke="var(--color-background)"
           strokeWidth={5}
@@ -101,7 +113,7 @@ function ScrollyElements({
           stroke="var(--color-background)"
           strokeWidth={5}
           className={styles.nameBorder}
-          d="M 10,30 L 190,30 L 190,170 L 10,170 Z"
+          d="M 10,5 L 190,5 L 190,195 L 10,195 Z"
         />
       </g>
     </svg>
--- src/components/Scrolly/index.tsx
+++ src/components/Scrolly/index.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useRef, useState, useCallback } from "react";
+import { useEffect, useRef, useState } from "react";
 import clsx from "clsx";
+import { useColorMode } from "../../context/ColorMode";
 import ScrollyElements from "./Elements";
 import styles from "./styles.module.css";
 
@@ -16,7 +17,6 @@ const jCenterYs = [jCenterOrig.y, 60] as const;
 const resizedJ = jSizeOrig.w * (cSizeOrig.h / jSizeOrig.h);
 const jSizeWs = [jSizeOrig.w, resizedJ * 0.8] as const;
 const jSizeHs = [jSizeOrig.h, cSizeOrig.h * 0.8] as const;
-const jColors = [0, 0, 255] as const;
 const cCenterXs = [cCenterOrig.x, 50] as const;
 const cCenterYs = [cCenterOrig.y, 140] as const;
 const cSizeWs = [cSizeOrig.w, cSizeOrig.w * 0.8] as const;
@@ -29,6 +29,7 @@ const textSizes = [64, 64 * 0.8] as const;
 const text1Opacities = [0, 0, 1] as const;
 const text2Opacities = [0, 0, 1] as const;
 const borderOpacities = [0, 1] as const;
+const bgOpacities = [0, 1] as const;
 
 function linearInterpolate(x: number) {
   return (keyframes: readonly number[]) => {
@@ -43,52 +44,82 @@ function linearInterpolate(x: number) {
 }
 
 function ScrollyClient() {
-  const [percentage, setPercentage] = useState(0);
   const [verticalDistance, setVerticalDistance] = useState(0);
-  const [svgRatio, setSvgRatio] = useState(1);
-  const [frameHeight, setFrameHeight] = useState(0);
-  const svgRef = useRef<SVGSVGElement>(null);
   const frameRef = useRef<HTMLDivElement>(null);
-  const onScroll = useCallback(() => {
-    const scrollPercentage =
-      Math.floor(window.scrollY / frameHeight * 1000) / 1000;
-    if (
-      scrollPercentage >= 0 &&
-      scrollPercentage <= 1 &&
-      scrollPercentage !== percentage
-    ) {
-      setPercentage(scrollPercentage);
-      setVerticalDistance(window.scrollY / svgRatio);
-    }
-  }, [svgRatio, frameHeight]);
-  const onResize = useCallback(() => {
-    setSvgRatio((svgRef.current?.getBoundingClientRect().width ?? 400) / 400);
-    setFrameHeight(frameRef.current?.getBoundingClientRect().height ?? 0);
-  }, []);
+  const svgRef = useRef<SVGSVGElement>(null);
+  const [percentage, setPercentage] = useState(0);
+  const getSVGRatio = () =>
+    (svgRef.current?.getBoundingClientRect().width ?? 400) / 400;
+
+  const { colorMode } = useColorMode();
+  const jColors = colorMode === "dark" ? [1, 1] : [0, 1];
+  const observer = useRef(
+    new IntersectionObserver(
+      (entries) => {
+        const p = entries[0]!.intersectionRatio;
+        const d = entries[0]!.intersectionRect.height;
+        if (
+          p >= 0 &&
+          p <= 1 &&
+          p !== percentage &&
+          entries[0]!.intersectionRect.top !== entries[0]!.rootBounds!.top
+        ) {
+          setPercentage(p);
+          setVerticalDistance(d);
+        }
+      },
+      { threshold: Array.from({ length: 1000 }, (_, i) => i / 1000) },
+    ),
+  );
   useEffect(() => {
+    observer.current.observe(frameRef.current!);
+    const onScroll = () => {
+      // Intersection observer doesn't reliably fire for the boundaries, so we
+      // add another handler (but IO still has better performance). It's also
+      // useful for the initial render
+      if (window.scrollY < 20) {
+        setPercentage(0);
+        setVerticalDistance(0);
+      } else if (window.scrollY > frameRef.current!.offsetTop) {
+        setPercentage(1);
+        setVerticalDistance(frameRef.current!.getBoundingClientRect().height);
+      }
+    }
+    onScroll();
     window.addEventListener("scroll", onScroll);
-    window.addEventListener("resize", onResize);
-    onResize();
     return () => {
+      observer.current.disconnect();
       window.removeEventListener("scroll", onScroll);
-      window.removeEventListener("resize", onResize);
     };
-  }, [onResize, onScroll]);
+  }, []);
   const getVal = linearInterpolate(percentage);
   return (
     <div className={styles.container}>
       {[0, 1].map((i) => (
         <div
-          className={clsx(styles.frame, i === 1 && styles.pictureFrame)}
+          className={clsx(styles.frame)}
           key={i}
-          ref={i === 0 ? frameRef : undefined}
+          ref={i === 1 ? frameRef : undefined}
         />
       ))}
+      <div
+        style={{
+          transform: `translateY(${verticalDistance}px)`,
+          opacity: getVal(bgOpacities),
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+        className={clsx(styles.frame, styles.pictureFrame)}
+      />
       <ScrollyElements
-        ref={svgRef}
         superTall
+        ref={svgRef}
         gScale={getVal(gScales)}
-        gShift={{ x: getVal(gShiftXs), y: getVal(gShiftYs) + verticalDistance }}
+        gShift={{
+          x: getVal(gShiftXs),
+          y: getVal(gShiftYs) + verticalDistance / getSVGRatio(),
+        }}
         jCenter={{ x: getVal(jCenterXs), y: getVal(jCenterYs) }}
         jSize={{ w: getVal(jSizeWs), h: getVal(jSizeHs) }}
         jColor={getVal(jColors)}
@@ -101,8 +132,8 @@ function ScrollyClient() {
         textSize={getVal(textSizes)}
         text1Opacity={getVal(text1Opacities)}
         text2Opacity={getVal(text2Opacities)}
-        borderOpacity={getVal(borderOpacities)}
-        borderShiftY={verticalDistance}
+        borderProgress={getVal(borderOpacities)}
+        borderShiftY={verticalDistance / getSVGRatio()}
       />
     </div>
   );
@@ -114,32 +145,33 @@ function ScrollyServer() {
       {([0, 1] as const).map((i) => {
         const getVal = linearInterpolate(i);
         return (
-        <div
-          className={clsx(styles.frame, i === 1 && styles.pictureFrame)}
-          key={i}>
-          <div className={styles.frameContent}>
-            <ScrollyElements
-              superTall={false}
-              gScale={getVal(gScales)}
-              gShift={{ x: getVal(gShiftXs), y: getVal(gShiftYs) }}
-              jCenter={{ x: getVal(jCenterXs), y: getVal(jCenterYs) }}
-              jSize={{ w: getVal(jSizeWs), h: getVal(jSizeHs) }}
-              jColor={getVal(jColors)}
-              cCenter={{ x: getVal(cCenterXs), y: getVal(cCenterYs) }}
-              cSize={{ w: getVal(cSizeWs), h: getVal(cSizeHs) }}
-              text1={i === 1 ? "oshua" : ""}
-              text2={i === 1 ? "hen" : ""}
-              text1Pos={{ x: getVal(text1Xs), y: getVal(text1Ys) }}
-              text2Pos={{ x: getVal(text2Xs), y: getVal(text2Ys) }}
-              textSize={getVal(textSizes)}
-              text1Opacity={getVal(text1Opacities)}
-              text2Opacity={getVal(text2Opacities)}
-              borderOpacity={getVal(borderOpacities)}
-              borderShiftY={0}
-            />
+          <div
+            className={clsx(styles.frame, i === 1 && styles.pictureFrame)}
+            key={i}>
+            <div className={styles.frameContent}>
+              <ScrollyElements
+                superTall={false}
+                gScale={getVal(gScales)}
+                gShift={{ x: getVal(gShiftXs), y: getVal(gShiftYs) }}
+                jCenter={{ x: getVal(jCenterXs), y: getVal(jCenterYs) }}
+                jSize={{ w: getVal(jSizeWs), h: getVal(jSizeHs) }}
+                jColor={getVal([0, 1])}
+                cCenter={{ x: getVal(cCenterXs), y: getVal(cCenterYs) }}
+                cSize={{ w: getVal(cSizeWs), h: getVal(cSizeHs) }}
+                text1={i === 1 ? "oshua" : ""}
+                text2={i === 1 ? "hen" : ""}
+                text1Pos={{ x: getVal(text1Xs), y: getVal(text1Ys) }}
+                text2Pos={{ x: getVal(text2Xs), y: getVal(text2Ys) }}
+                textSize={getVal(textSizes)}
+                text1Opacity={getVal(text1Opacities)}
+                text2Opacity={getVal(text2Opacities)}
+                borderProgress={getVal(borderOpacities)}
+                borderShiftY={0}
+              />
+            </div>
           </div>
-        </div>
-      );})}
+        );
+      })}
     </div>
   );
 }
